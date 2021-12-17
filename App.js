b07088147997d28392e46df065bea59c1a91e288
Zack <zackbana@gmail.com>
2021-12-17 20:26:02 +0800

    add touchableopacity

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, Text, View, FlatList } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+
+function HomeScreen() {
+ const [colorArray, setColorArray] = useState([
+   { red: 255, green: 0, blue: 0, id: "0" },
+   { red: 0, green: 255, blue: 0, id: "1" },
+   { red: 0, green: 0, blue: 255, id: "2" },
+ ]);
+
+ function renderItem({ item }) {
+   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+ }
+
+ function addColor() {
+  setColorArray([
+    ...colorArray,
+    {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: `${colorArray.length}`,
+    },
+  ]);
+}
+
+return (
+  <View style={styles.container}>
+    <TouchableOpacity
+      style={{ height: 40, justifyContent: "center" }}
+      onPress={addColor}
+    >
+      <Text style={{ color: "red" }}>Add colour</Text>
+    </TouchableOpacity>
+    <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+  </View>
+);
+
+
+//  return (
+//    <View style={styles.container}>
+//      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+//    </View>
+//  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Colour List" component={HomeScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
-//comments
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+ },
+ list: {
+   width: "100%",
+ },
 });
